65f863dd1bc445f135d835102454048b68a32faa
nao20010128nao <nao20010128@gmail.com>
2017-12-01 22:22:01 +0900

    修正

--- dep.sh
+++ dep.sh
@@ -2,8 +2,8 @@
 
 # This script used to pre-download all dependencies.
 
-if groovy -e "$(cat *.groovy | grep Grab) import groovy.*" then;
-  echo "Dependencies are properly downloaded. You can go ahead"
+if groovy -e "$(cat *.groovy | grep Grab) import groovy.*"; then
+  echo "Dependencies are properly downloaded. You can go ahead."
 else
   echo -n "Dependencies may not be downloaded correctly.\nReport about above."
 fi
